623638fbca4663927511a6d3a028a9890f50dec5
Okkun555 <y.okuno.btm@gmail.com>
2024-06-30 14:39:38 +0900

    config:テスト環境にSQLiteを利用する

--- config/database.php
+++ config/database.php
@@ -109,6 +109,12 @@ return [
             // 'trust_server_certificate' => env('DB_TRUST_SERVER_CERTIFICATE', 'false'),
         ],
 
+        'testing_sqlite' => [
+            'driver' => 'sqlite',
+            'database' => ':memory:',
+            'prefix' => '',
+        ],
+
     ],
 
     /*
